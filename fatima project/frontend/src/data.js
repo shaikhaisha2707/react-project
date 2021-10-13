e4648ea263d03e216eec7beaf789dd43a5c8e040
Aisha <shaikhash321@gmail.com>
2021-10-13 14:31:06 +0530

    saved

---
+++ fatima project/frontend/src/data.js
@@ -0,0 +1,63 @@
+export default {
+    products: [
+      {
+        _id: '1',
+        name: 'Floral Scents',
+        category: 'Women',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: ' Chanel',
+        rating: 4.5,
+        numReviews: 10
+      },
+      {
+        _id: '2',
+        name: 'Woody Scents',
+        category: 'Men',
+        image: '/images/d2.jpg',
+        price: 80,
+        brand: 'Armani',
+        rating: 4.2,
+        numReviews: 5
+      },
+      {
+        _id: '3',
+        name: 'Oriental Scents',
+        category: 'Men',
+        image: '/images/d3.jpg',
+        price: 77,
+        brand: 'Gucci',
+        rating: 4.5,
+        numReviews: 8
+      }, {
+        _id: '4',
+        name: 'Fresh Scents',
+        category: 'Women',
+        image: '/images/d4.jpg',
+        price: 100,
+        brand: 'Versace',
+        rating: 4.5,
+        numReviews: 8
+      },
+      {
+        _id: '5',
+        name: 'Spicy Scents',
+        category: 'Men',
+        image: '/images/d5.jpg',
+        price: 60,
+        brand: 'Dior',
+        rating: 4.5,
+        numReviews: 8
+      },
+      {
+        _id: '6',
+        name: 'Aromatic Scents',
+        category: 'Women',
+        image: '/images/d6.jpg',
+        price: 50,
+        brand: 'Tommy Hilfiger',
+        rating: 4.5,
+        numReviews: 8
+      },
+    ]
+  }
---
+++ fatima project/frontend/src/screens/ProductScreen.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import data from '../data';
+
+
+
+function ProductScreen(props) {
+    console.log(props.match.params.id);
+    const product=data.products.find(x=> x._id === props.match.params.id);
+    return <div>
+        <div className="back-to-result">
+            <Link to="/">Back to result</Link>
+        </div>
+        <div className="details">
+            <div className="details-image">
+              <img src={product.image} alt="product"></img>
+            </div>
+            <div className="details-info">
+              <ul>
+                <li>
+                  <h4>{product.name}</h4>
+                </li>
+                <li>
+                  <a href="#reviews">
+                    <Rating
+                      value={product.rating}
+                      text={product.numReviews + ' reviews'}
+                    />
+                  </a>
+                </li>
+                <li>
+                  Price: <b>${product.price}</b>
+                </li>
+                <li>
+                  Description:
+                  <div>{product.description}</div>
+                </li>
+              </ul>
+            </div>
+            <div className="details-action">
+              <ul>
+                <li>Price: {product.price}</li>
+                <li>
+                  Status:{' '}
+                  {product.countInStock > 0 ? 'In Stock' : 'Unavailable.'}
+                </li>
+                <li>
+                  Qty:{' '}
+                  <select
+                    value={qty}
+                    onChange={(e) => {
+                      setQty(e.target.value);
+                    }}
+                  >
+                    {[...Array(product.countInStock).keys()].map((x) => (
+                      <option key={x + 1} value={x + 1}>
+                        {x + 1}
+                      </option>
+                    ))}
+                  </select>
+                </li>
+                <li>
+                  {product.countInStock > 0 && (
+                    <button
+                      onClick={handleAddToCart}
+                      className="button primary"
+                    >
+                      Add to Cart
+                    </button>
+                  )}
+                </li>
+              </ul>
+            </div>
+        </div>
+        
+    </div>
+   
+
+
+}
+export default ProductScreen;
